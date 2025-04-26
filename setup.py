bb5aba7e134ae072b72e3f6f77578f2a1c35efca
nikhil <nihkilmogre1998@gmail.com>
2025-04-27 01:05:10 +0530

    data ingestion with api for astradb

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name="e-commercebot",
+      version="0.0.1",
+      author="nikhil",
+      author_email="nikhilmogre1998@gmail.com",
+      packages=find_packages(),
+      install_requires=['langchain-astradb','langchain'])
